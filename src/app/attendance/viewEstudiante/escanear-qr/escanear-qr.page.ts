e7046182ff37ebe8364e0e3c509d0a2950f444d3
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-11-28 20:32:55 -0300

    Android

--- src/app/attendance/viewEstudiante/escanear-qr/escanear-qr.page.ts
+++ src/app/attendance/viewEstudiante/escanear-qr/escanear-qr.page.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AttendanceService } from 'src/app/services/attendance.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ZBarOptions, ZBar } from '@ionic-native/zbar/ngx';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-escanear-qr',
@@ -17,43 +17,48 @@ import { ZBarOptions, ZBar } from '@ionic-native/zbar/ngx';
   imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class EscanearQrPage implements OnInit {
-  optionZbar: any;
-  scannedOutput: any;
+  isSupported = false;
+  barcodes: Barcode[] = [];
 
-  constructor(private router: Router, private fb: FormBuilder, private attServ: AttendanceService, private zbarPlugin: ZBar) {
-    this.optionZbar = {
-      flash: 'off',
-      drawSight: false
-    }
-  }
-
-  barcodeScanner(){
-    this.zbarPlugin.scan(this.optionZbar)
-   .then(respone => {
-      console.log(respone);
-      this.scannedOutput = respone;
-   })
-   .catch(error => {
-      alert(error);
-   });
+  constructor(private router: Router, private fb: FormBuilder, private attServ: AttendanceService, private alertController: AlertController) {
   }
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
   }
 
   onSubmit() {
-
-    if (true) {
-      const uuid = this.scannedOutput.get('uuid')?.value;
-      if (uuid) {
-        this.attServ.guardarAsistencia(uuid);
-      }
-    }
 
   }
 
   goBack() {
     this.router.navigate(['/home']); // O la ruta de tu página principal
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
 
